551395a1072d2fec129c194af8ad92f0bd343c7d
Sakshi Nitnawre <chmcs2026.sakshi.nitnawre@gmail.com>
2026-03-05 18:47:48 +0530

    fix api connection

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://campus-lost-found-frontend-tle2.onrender.com"
+  baseURL: "https://campus-lost-found-backend-tvn7.onrender.com"
 });
 
 export default api;
