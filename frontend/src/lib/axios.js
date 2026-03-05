ca8c240a98c61f3c4f9cec94448a8b598aebce81
Sakshi Nitnawre <chmcs2026.sakshi.nitnawre@gmail.com>
2026-03-05 18:57:23 +0530

    Fix API base URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://campus-lost-found-backend-tvn7.onrender.com/api"
+  baseURL: "https://campus-lost-found-backend-tvn7.onrender.com/"
 });
 
 export default api;
